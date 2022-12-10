9b326922d2a90cea130c538602519d6a2cd7d7be
Seliutin-Roman-UA <selutin@gmail.com>
2022-12-10 15:48:29 +0200

    fin

---
+++ js/data.js
@@ -0,0 +1,174 @@
+export const content = {
+  header_head: {
+    ua: 'Селютін Роман',
+    eng: 'Seliutin Roman',
+  },
+  header_text: {
+    ua: 'JUNIOR FRONTEND DEVELOPER',
+    eng: 'JUNIOR FRONTEND DEVELOPER',
+  },
+  contacts_caption: {
+    ua: 'Контакти:',
+    eng: 'Contacts:',
+  },
+  contacts_location: {
+    ua: 'Одеса, Україна',
+    eng: 'Odesa, Ukraine',
+  },
+  techskills_caption: {
+    ua: 'Технічні навички:',
+    eng: 'Tech skills:',
+  },
+  softskills_caption: {
+    ua: 'Soft skills:',
+    eng: 'Soft skills:',
+  },
+  softskills_one: {
+    ua: 'Адекватній та дорослий',
+    eng: 'Adequate and adult',
+  },
+  softskills_two: {
+    ua: 'Вимогливий до себе',
+    eng: 'Demanding to himself',
+  },
+  softskills_three: {
+    ua: 'Визнаю помилки',
+    eng: 'Admit mistakes',
+  },
+  softskills_four: {
+    ua: 'Люблю вчитися та вирішувати "задачки з зірочкой"',
+    eng: 'Like to learn and solve "problems with an asterisk"',
+  },
+  language_caption: {
+    ua: 'Володіння мовами',
+    eng: 'Languages',
+  },
+  language_eng: {
+    ua: 'Англійська - Intermediate',
+    eng: 'English - Intermediate',
+  },
+  language_ua: {
+    ua: 'Укhаїнська - вільно',
+    eng: 'Ukrainian - fluently',
+  },
+  language_ru: {
+    ua: 'Російська - вільно',
+    eng: 'Russian - fluently',
+  },
+  hero_title: {
+    ua: 'Головне',
+    eng: 'Summary',
+  },
+  hero: {
+    ua: 'Шукаю роботу Front-End developer на повний робочий час (розгляну неповну зайнятость). Маю впевнені знання в HTML, CSS, JavaScript, and React. Зараз вивичаю NODE.js, Vie, TS. Маю знання Agile/Scrum, які здобув при роботі на командних проектах. Потребую роботу, на який можно розвивати технічні навички.',
+    eng: 'I am looking for a full-time Front-End developer job (will consider part-time). I have solid knowledge of HTML, CSS, JavaScript, and React. Now I am learning NODE.js, Vie, TS. I have knowledge of Agile/Scrum, which I gained while working on team projects. I need a job where I can develop my technical skills.',
+  },
+  projects_title: {
+    ua: 'Проекти',
+    eng: 'Project experience',
+  },
+  projects_wallet_about: {
+    ua: 'застосунок для ведення обліку власного бюджету',
+    eng: 'an application for keeping track of your own budget',
+  },
+  projects_wallet_description: {
+    ua: 'projects_wallet_description',
+    eng: 'Вебсайт з адаптивною версткою, модальнимі вікнами, працює з Local Storage та бекендом.',
+  },
+  projects_wallet_team: {
+    ua: 'Командний проект.',
+    eng: 'Team project.',
+  },
+  projects_coctail_about: {
+    ua: 'застосунок з рецептурами коктейлів',
+    eng: 'an application with cocktail recipes',
+  },
+  projects_coctail_description: {
+    ua: 'Вебсайт з адаптивною версткою, модальнимі вікнами, бургер меню, працює з Local Storage та ,екендом.',
+    eng: 'Website with responsive layout, modal windows, burger menu, works with Local Storage and backend.',
+  },
+  projects_coctail_team: {
+    ua: 'Командний проект.',
+    eng: 'Team project.',
+  },
+  projects_donut_about: {
+    ua: 'вебсайт кафе та школи з виготовлення пончиків',
+    eng: 'the website of the cafe and donut making trainings',
+  },
+  projects_donut_description: {
+    ua: 'Вебсайт з адаптивною версткою, модальнимі вікнами, бургер меню, кастомна пагінація відгугів.',
+    eng: 'Responsive layout website, modal windows, burger menu, custom feedback pagination.',
+  },
+  projects_donut_team: {
+    ua: 'Індивідуальний проект.',
+    eng: 'Individual project.',
+  },
+  projects_icecream_about: {
+    ua: 'вебсайт мережі кафе',
+    eng: 'website of a chain of cafes',
+  },
+  projects_icecream_description: {
+    ua: 'Вебсайт з адаптивною версткою, модальнимі вікнами, бургер меню.',
+    eng: 'Responsive layout website, modal windows, burger menu, custom feedback pagination.',
+  },
+  projects_icecream_team: {
+    ua: 'Командний проект.',
+    eng: 'Team project.',
+  },
+  projects_webstudio_about: {
+    ua: 'вебсайт компанії, що надає послуги web-дизайну',
+    eng: 'website of a company that provides web-design services',
+  },
+  projects_webstudio_description: {
+    ua: 'Багатосторінковий вебсайт з адаптивною версткою, модальнимі вікнами, бургер меню.',
+    eng: 'Multi-page website with responsive layout, modal windows, burger menu.',
+  },
+  projects_webstudio_team: {
+    ua: 'Командний проект.',
+    eng: 'Team project.',
+  },
+  experience_title: {
+    ua: 'Досвід роботи',
+    eng: 'Work experience',
+  },
+  experience_summary: {
+    ua: 'Донедавна професійно займався корпоративними фінансами на посадах фінансовий директор, головнйи бухгалтер. Маю значнйи доcвід в реалізації задач з автоматизації обліку, бізнес-процесів в ролі менеджера проекту на боці замовника. Maю досвід з керуванн командою до 50 членів, розумію вартість слів "результат" та "дедлайн"',
+    eng: 'Until recently, he was professionally engaged in corporate finance in the positions of financial director, chief accountant. I have significant experience in the implementation of accounting automation tasks, business processes in the role of a project manager on the customer\'s side. I have experience in managing a team of up to 50 members, I understand the value of the words "result" and "deadline."',
+  },
+  experience_company_caption: {
+    ua: 'Компанії з якими працював:',
+    eng: 'Places of work:',
+  },
+  experience_tp: {
+    ua: 'ТОВ «Телекарт-Прилад» (м. Одеса) (завод з виробництва електронної продукції). Обіймав посаду фінансового директора і головного бухгалтера (за сумісництвом).',
+    eng: 'LLC "Telekart-Prylad" (Odesa) (plant for the production of electronic products). I held the position of financial director and chief accountant (part-time).',
+  },
+  experience_mc: {
+    ua: 'ТОВ «Мед-Сервіс» (м. Дніпро) (національна мережа фармацевтичного рітейла). Обіймав посаду фінансового директора. В цієї компанії втілив цілу низку проектів обліку та корпоративного управління.',
+    eng: '"Med-Service" LLC (Dnipro) (national pharmaceutical retail chain). I held the position of financial director. In this company, I implemented a number of accounting and corporate management projects.',
+  },
+  experience_fram: {
+    ua: 'ТОВ «ФРА-М» (м. Донецьк) (національний дистрібутор фармакологічної продукції)ю Обіймав посаду фінансового директора. Велику частину роботі займало управління власним та запозиченим капіталом',
+    eng: '"FRA-M" LLC (Donetsk) (national distributor of pharmacological products). I held the position of financial director. A large part of the work was occupied by the management of own and borrowed capital',
+  },
+  experience_baget: {
+    ua: 'ТОВ «Ювелірний завод «Багет» (м. Донецьк) (великий Український виробник ювелірних виробів). Обіймав посаду фінансового директора. Тут втілив низку проектів з обліку виробництва.',
+    eng: 'LLC "Jewelry factory "Baget" (Donetsk) (a large Ukrainian manufacturer of jewelry). I held the position of financial director. Here I implemented a number of production accounting projects.',
+  },
+  education_title: {
+    ua: 'Освіта',
+    eng: 'Education',
+  },
+  education_goit: {
+    ua: '(2023) IT школа GoIT.',
+    eng: '(2023) GoIT - school of IT',
+  },
+  education_1c: {
+    ua: '(2015) Курсы 1С:Програміст.',
+    eng: '(2015) Courses 1C: Programmer.',
+  },
+  education_univer: {
+    ua: '(1995-2000) Донецький політех. Eкономіка підприємства.',
+    eng: '(1995-2000) Donetsk Polytechnic. Enterprise economy.',
+  },
+};
---
+++ js/script.js
@@ -0,0 +1,27 @@
+import { content } from './data.js';
+
+function changeLanguage(len) {
+  console.log(content.header_head[len]);
+  for (let i in content) {
+    document.querySelector(`.${i}`).textContent = content[i][len];
+  }
+}
+document.querySelector('.leng').addEventListener('change', e => {
+  document.querySelector('.leng').classList.toggle('flag_uk');
+  changeLanguage(e.target.value);
+});
+
+document
+  .querySelector("[name='fist']")
+  .addEventListener('input', e =>
+    document.documentElement.style.setProperty('--dark', e.target.value)
+  );
+document
+  .querySelector("[name='second']")
+  .addEventListener('input', e =>
+    document.documentElement.style.setProperty('--light', e.target.value)
+  );
+document.querySelector('.palitra').addEventListener('click', () => {
+  document.querySelector('.color-menu').classList.toggle('move');
+  document.querySelector('.palitra').classList.toggle('rotate');
+});
